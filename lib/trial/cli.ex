86c5564a124fc76f8740f34ecc9e4af52b5b3e13
Dennis Peterson <denispeterson96@gmail.com>
2019-04-25 11:42:10 +0300

    Done with client version

---
+++ lib/trial/cli.ex
@@ -0,0 +1,28 @@
+defmodule Trial.CLI do
+  def main(args) do
+    parse_args(args)
+    |> process
+  end
+  def parse_args(args) do
+    parse = OptionParser.parse(args, switches: [help: :boolean],
+            aliases: [h: :help])
+
+    case parse do
+    {[help: true], _, _} -> :help
+    {_, [url], _} -> {url}
+    end
+  end
+
+  def process({url}) do
+    HttPoison.JSONFetch.fetchtodos(url)
+    |> HttPoison.ExtractData.extract_required  # Extracting data
+  end
+  def process(:help) do
+    IO.puts """
+    trial
+    ---------------
+    Usage: trial <url>
+    Example: trial https://jsonplaceholder.typicode.com/todos/1
+    """
+  end
+end
